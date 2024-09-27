e046cb4d94e164433aaa292b50a0a473cf58a692
Alex <sasha.morozov022@yandex.ru>
2024-09-27 23:58:14 +0300

    a little change models in app models

--- message_board/models/models.py
+++ message_board/models/models.py
@@ -6,4 +6,8 @@ from django.contrib.auth.models import User
 class User_board(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     
-    
+class message_board(models.Model):
+    author = models.ForeignKey(User_board,models.CASCADE)
+
+    header = models.CharField(max_length=64)
+    text = models.TextField(max_length=1000)
